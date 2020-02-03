b1f8a77b979d44c3a5ef662df7cac2f636c11638
Loh Eng Liang <lohengliang@gmail.com>
2020-02-03 18:23:30 +0800

    Updates

---
+++ precache-manifest.36456d89da63ee5a874b5aa6385aa16a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68eb25356379c7f3d366305b44ef7cf",
+    "url": "/lohengliang/the-straits-times-clone/index.html"
+  },
+  {
+    "revision": "ea2430317d359b5156de",
+    "url": "/lohengliang/the-straits-times-clone/static/css/main.939f5371.chunk.css"
+  },
+  {
+    "revision": "a4331afb3a12e561fd38",
+    "url": "/lohengliang/the-straits-times-clone/static/js/2.ef2c8d5d.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/lohengliang/the-straits-times-clone/static/js/2.ef2c8d5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea2430317d359b5156de",
+    "url": "/lohengliang/the-straits-times-clone/static/js/main.18155c7b.chunk.js"
+  },
+  {
+    "revision": "ebe85598dfe045de6480",
+    "url": "/lohengliang/the-straits-times-clone/static/js/runtime-main.f2d94aed.js"
+  }
+]);
